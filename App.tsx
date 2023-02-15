778da784c7c3528bf827733c2c1f737848eef040
SaulMoreyra <armhacam21@gmail.com>
2023-02-15 17:37:29 -0600

    feat: implement example of reanimated

--- App.tsx
+++ App.tsx
@@ -1,9 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  withRepeat,
+} from "react-native-reanimated";
+
+const SIZE = 100;
+
+const handleRotation = (progress: Animated.SharedValue<number>) => {
+  "worklet";
+  return `${progress.value * 2 * Math.PI}rad`;
+};
 
 export default function App() {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      borderRadius: (progress.value * SIZE) / 2,
+      opacity: progress.value,
+      transform: [{ scale: scale.value }, { rotate: handleRotation(progress) }],
+    };
+  }, []);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), 3, true);
+    scale.value = withRepeat(withSpring(1), 3, true);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Animated.View
+        style={[
+          { height: SIZE, width: SIZE, backgroundColor: "blue" },
+          reanimatedStyle,
+        ]}
+      ></Animated.View>
     </View>
   );
 }
@@ -11,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
